337c8845fe09da7143b31894b1751972bedd5fcc
PavloZubovych <zu10@ukr.net>
2022-08-26 22:41:50 +0300

    start project toDoList

---
+++ main.js
@@ -0,0 +1,66 @@
+const addTaskBtn = document.getElementById('add-task-btn');
+const deskTaskInput = document.getElementById('description-task');
+const todosWrapper = document.querySelector('.todos-wrapper');
+
+let tasks;
+
+!localStorage.tasks
+  ? (tasks = [])
+  : (tasks = JSON.parse(localStorage.getItem('tasks')));
+
+let todoItemElements = [];
+
+function Task(description) {
+  this.description = description;
+  this.completed = false;
+}
+
+const createTamplate = (task, index) => {
+  return `<div class="todo-item ${task.completed ? 'checked' : ''}">
+      <div class="descreption">${task.description}</div>
+      <div class="buttons">
+        <input onclick="completeTask(${index})" type="checkbox" class="btn-complete" ${
+    task.completed ? 'checked' : ''
+  }>
+        <button class="btn-deleten">Delete</button>
+      </div>
+    </div>`;
+};
+
+const fillHtmlList = () => {
+  //   console.log('befor', todosWrapper);
+  todosWrapper.innerHTML = '';
+  if (tasks.length > 0) {
+    tasks.forEach((item, index) => {
+      todosWrapper.innerHTML += createTamplate(item, index);
+    });
+    todoItemElements = document.querySelectorAll('.todo-item');
+  }
+  //   console.log('after', todosWrapper);
+};
+
+fillHtmlList();
+
+const updateLocal = () => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const completeTask = (index) => {
+  tasks[index].completed = !tasks[index].completed;
+  if (tasks[index].completed) {
+    console.log(todoItemElements[index]);
+    todoItemElements[index].classList.add('checked');
+  } else {
+    todoItemElements[index].classList.remove('cheked');
+  }
+  updateLocal();
+  fillHtmlList();
+};
+
+addTaskBtn.addEventListener('click', () => {
+  tasks.push(new Task(deskTaskInput.value));
+  updateLocal();
+  fillHtmlList();
+  //   console.log(tasks);
+  deskTaskInput.value = '';
+});
